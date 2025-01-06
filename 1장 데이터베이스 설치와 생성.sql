a19ecc935a636f26095c4166f63b8319e3f61da6
wnsdud060451 <wnsdud060451@gmail.com>
2025-01-06 17:37:18 +0900

    1장 데이터베이스 실습

---
+++ 1장 데이터베이스 설치와 생성.sql
@@ -0,0 +1,17 @@
+# 날짜: 2025/01/06
+# 이름: 변준영
+# 내용: 1장 데이터베이스 설치와 생성
+
+#실습 1-1
+CREATE DATABASE `StudyDB1`;
+DROP DATABASE `StudyDB`;
+
+#실습 1-2
+CREATE USER 'wnsdud060451'@'%' identified by '1234';
+GRANT ALL privileges on StudyDB.* to 'wnsdud060451'@'%';
+flush privileges;
+
+#실습 1-3
+alter user 'wnsdud060451'@'%' identified by 'abc1234';
+drop user 'wnsdud060451'@'%';
+flush privileges;
